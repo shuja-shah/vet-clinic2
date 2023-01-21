535c20108e8dea1cf8882e9344c013fc1b849919
shuja-shah <shujashah786@gmail.com>
2023-01-22 00:22:50 +0500

    Add Transactions

--- queries.sql
+++ queries.sql
@@ -9,3 +9,40 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = TRUE;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+
+/* TRANSACTIONS */
+BEGIN;
+UPDATE animals SET species='unspecified';
+select * from animals;
+ROLLBACK;
+select * from animals;
+
+BEGIN;
+UPDATE animals SET species='digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species='pokemon' WHERE species IS NULL;
+COMMIT;
+
+/* DROP TRANSACTIONS */
+BEGIN;
+DELETE FROM animals;
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE from animals where date_of_birth>'2022-01-01';
+SAVEPOINT sp1;
+UPDATE animals SET weight_kg=weight_kg*-1;
+ROLLBACK to sp1;
+UPDATE animals SET weight_kg=weight_kg*-1 WHERE weight_kg<0;
+COMMIT;
+
+
+
+/* Queries and Answers */
+
+/* 1. How many animals are there? */
+SELECT COUNT(*) FROM animals;
+/* 2. How many animals have never tried to escape? */
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;

f132c2b682d99eb27de3e362dd0fff1fcca89318
shuja-shah <shujashah786@gmail.com>
2023-01-22 00:26:55 +0500

    Answer all queries

--- queries.sql
+++ queries.sql
@@ -46,3 +46,11 @@ COMMIT;
 SELECT COUNT(*) FROM animals;
 /* 2. How many animals have never tried to escape? */
 SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+/* 3. What is the average weight of animals? */
+SELECT AVG(weight_kg) FROM animals;
+/* 4. Who escapes the most, neutered or not neutered animals? */
+SELECT MAX(escape_attempts),neutered from animals GROUP BY neutered;
+/* 5. What is the minimum and maximum weight of each type of animal? */
+SELECT MIN(weight_kg),MAX(weight_kg),species FROM animals GROUP BY species;
+/*6. What is the average number of escape attempts per animal type of those born between 1990 and 2000? */
+SELECT AVG(escape_attempts),species FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
